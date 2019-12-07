3d0af5e914bd74f91b2dd106f82c9d5999e4f956
jlmcabral <1140471@isep.ipp.pt>
2019-12-07 19:42:14 +0000

    Header added (without styles). Created in a php function

--- WWW/cinem/lab11/contacts.php
+++ WWW/cinem/lab11/contacts.php
@@ -5,28 +5,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Microgreens Porto</title>
+    <title>Microgreens Porto - Contactos</title>
     <link rel="stylesheet" href="styles/home.css">
 </head>
 <body>
 
 <?php printHeaderMenu();?>
-
-    <div class="center-piece">
-        <img src="img/home-plant-med-no-prev.png" alt="Home microgreen">
-
-
-
-        <!-- <picture>
-            <source media="(min-width: 650px)" srcset="img_pink_flowers.jpg">
-            <source media="(min-width: 465px)" srcset="img_white_flower.jpg">
-            <img src="img_orange_flowers.jpg" alt="Flowers" style="width:auto;">
-        </picture> -->
-    </div>
-
+    
 <?php
+    // include("functions.php");
 
 
+    print "<h1>Contactos</h1>\n";
+    // $conn = ligaDB();
+    // listProdutos(ligaDB());
+    
 
 ?>
 
---
+++ WWW/cinem/lab11/header.php
@@ -0,0 +1,21 @@
+
+<?php
+
+// Prints a navigation main menu
+// Just for reusability purposes
+function printHeaderMenu()
+{
+    print
+    "
+        <div class='header-menu'>
+            <ul>
+                <li><a href='index.php'>P&aacutegina Inicial</a></li>
+                <li><a href='produtos.php'>Produtos</a></li>
+                <li><a href='contacts.php'>Contactos</a></li>
+                <li><a href='user.php'>Conta de Utilizador</a></li>
+            </ul>
+        </div>
+    ";
+}
+
+?> 
